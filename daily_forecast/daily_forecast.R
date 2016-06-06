1befe8fa9e00385117f7575fc07d58b0d2394a21
Bryce Mecum <mecum@nceas.ucsb.edu>
2016-06-06 10:12:36 -0800

    Fix bug I introduced with the last set of changes

--- daily_forecast/daily_forecast.R
+++ daily_forecast/daily_forecast.R
@@ -22,6 +22,9 @@ estimated$pccpue <- estimated$ccpue / final_ccpue
 
 write.table(subset(estimated, day >= 1)$pccpue * 100, row.names = FALSE, col.names = FALSE, file = "daily_forecast/estimated.csv")
 
+# Subset the columns for later use
+estimated <- estimated[,c("day", "pccpue")]
+
 # Plot
 logistic_curve$date <- as.Date(logistic_curve$date)
 
